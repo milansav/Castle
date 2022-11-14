2de69f74da618e8d95fdd40ee67e7c499315ef28
Milan Savickij <contact@milansav.eu>
2022-11-14 15:17:24 +0100

    Add more complex numbers support to lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -25,20 +25,72 @@ type Lexeme struct {
 type LexemeType int
 
 const (
+	//Arithmetic operators
 	LT_PLUS LexemeType = iota
 	LT_MINUS
 	LT_MULTIPLY
 	LT_DIVIDE
 	LT_MODULO
+	LT_POWER
+
+	//Parentheses
 	LT_LPAREN
 	LT_RPAREN
+	LT_LCURLY
+	LT_RCURLY
+
+	//Binary operations
 	LT_BANG
-	LT_KEYWORD
+
 	LT_LITERAL
 	LT_NUMBER
+
+	//Keywords
+	LT_CONST
+	LT_IF
+	LT_ELSE
+	LT_ELSEIF
+
+	//Misc operators
+	LT_LAMBDA
+
 	LT_NONE
 	LT_END
 )
+
+var LexemeTypeLabels = map[LexemeType]string{
+	//Arithmetic operators
+	LT_PLUS:     "LT_PLUS",
+	LT_MINUS:    "LT_MINUS",
+	LT_MULTIPLY: "LT_MULTIPLY",
+	LT_DIVIDE:   "LT_DIVIDE",
+	LT_MODULO:   "LT_MODULO",
+	LT_POWER:    "LT_POWER",
+
+	//Parentheses
+	LT_LPAREN: "LT_LPAREN",
+	LT_RPAREN: "LT_RPAREN",
+	LT_LCURLY: "LT_LCURLY",
+	LT_RCURLY: "LT_RCURLY",
+
+	//Binary operations
+	LT_BANG: "LT_BANG",
+
+	LT_LITERAL: "LT_LITERAL",
+	LT_NUMBER:  "LT_NUMBER",
+
+	//Keywords
+	LT_CONST:  "LT_CONST",
+	LT_IF:     "LT_IF",
+	LT_ELSE:   "LT_ELSE",
+	LT_ELSEIF: "LT_ELSEIF",
+
+	//Misc operators
+	LT_LAMBDA: "LT_ELSEIF",
+
+	LT_NONE: "LT_NONE",
+	LT_END:  "LT_END",
+}
 
 func Create(source string) Lexer {
 
@@ -94,8 +146,11 @@ func number(lexer *Lexer) Lexeme {
 
 	start := lexer.currentStep
 
-	for unicode.IsDigit(currentRune(lexer)) {
+	c := currentRune(lexer)
+
+	for unicode.IsDigit(c) || c == ',' {
 		step(lexer)
+		c = currentRune(lexer)
 	}
 
 	end := lexer.currentStep
@@ -114,23 +169,27 @@ func other(lexer *Lexer) Lexeme {
 
 	lexeme := Lexeme{Label: string(target), Type: LT_NONE}
 
-	if target == '+' {
+	switch target {
+	case '+':
 		lexeme.Type = LT_PLUS
-		//fmt.Println("Is plus")
-	} else if target == '-' {
+	case '-':
 		lexeme.Type = LT_MINUS
-		//fmt.Println("Is minus")
-	} else if target == '*' {
+	case '*':
 		lexeme.Type = LT_MULTIPLY
-		//fmt.Println("Is multiply")
-	} else if target == '/' {
+	case '/':
 		lexeme.Type = LT_DIVIDE
-		//fmt.Println("Is divide")
-	} else if target == '(' {
+	case '%':
+		lexeme.Type = LT_MODULO
+	case '^':
+		lexeme.Type = LT_POWER
+	case '(':
 		lexeme.Type = LT_LPAREN
-		//fmt.Println("Is left parentheses")
-	} else if target == ')' {
+	case ')':
 		lexeme.Type = LT_RPAREN
+	case '{':
+		lexeme.Type = LT_LCURLY
+	case '}':
+		lexeme.Type = LT_RCURLY
 	}
 
 	step(lexer)
